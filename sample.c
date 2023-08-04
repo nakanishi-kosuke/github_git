f2fb16f55e49c5f1c301ea9c08d410776baaf549
Nakanishi512 <128363170+Nakanishi512@users.noreply.github.com>
2023-08-04 15:18:11 +0900

    Add files via upload

---
+++ sample.c
@@ -0,0 +1,33 @@
+/*sample.c*/ 
+ 
+/* @wi YMC-957 */ 
+ 
+int DemoPolarion=1234; 
+ 
+void test1(void) 
+{ 
+    printf("Header reading\n"); 
+} 
+ 
+int DemoPolarion=1234; 
+ 
+void test2(void) 
+{ 
+    printf("Header reading\n"); 
+} 
+ 
+ 
+int DemoPolarion=1234; 
+ 
+void test3(void) 
+{ 
+    printf("Header reading\n"); 
+} 
+ 
+ 
+int DemoPolarion=1234; 
+ 
+void test4(void) 
+{ 
+    printf("Header reading\n"); 
+} 
